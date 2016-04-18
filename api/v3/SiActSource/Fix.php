fe35ec33c18d6bf4e8c12e415e5bd0b031b05899
Erik Hommel <hommel@ee-atwork.nl>
2016-04-18 16:13:37 +0200

    scheduled job to fix duplicate recruiters and move their activities

--- api/v3/SiActSource/Fix.php
+++ api/v3/SiActSource/Fix.php
@@ -1,19 +1,6 @@
 <?php
-
 /**
- * SiActSource.Fix API specification (optional)
- * This is used for documentation and validation.
- *
- * @param array $spec description of fields supported by this API call
- * @return void
- * @see http://wiki.civicrm.org/confluence/display/CRMDOC/API+Architecture+Standards
- */
-function _civicrm_api3_si_act_source_Fix_spec(&$spec) {
-  $spec['magicword']['api.required'] = 1;
-}
-
-/**
- * SiActSource.Fix API
+ * SiActSource.Fix API Used to change the activity source for double recruiters
  *
  * @param array $params
  * @return array API result descriptor
@@ -22,19 +9,47 @@ function _civicrm_api3_si_act_source_Fix_spec(&$spec) {
  * @throws API_Exception
  */
 function civicrm_api3_si_act_source_Fix($params) {
-  if (array_key_exists('magicword', $params) && $params['magicword'] == 'sesame') {
-    $returnValues = array( // OK, return several data rows
-      12 => array('id' => 12, 'name' => 'Twelve'),
-      34 => array('id' => 34, 'name' => 'Thirty four'),
-      56 => array('id' => 56, 'name' => 'Fifty six'),
-    );
-    // ALTERNATIVE: $returnValues = array(); // OK, success
-    // ALTERNATIVE: $returnValues = array("Some value"); // OK, return a single value
+  $returnValues = array("Recruiters deduped and activity source contact fixed: ");
+  $query = 'SELECT DISTINCT(sort_name) FROM civicrm_contact WHERE contact_sub_type LIKE %1';
+  $dao = CRM_Core_DAO::executeQuery($query, array(1 => array('%recruiter%', 'String')));
+  while ($dao->fetch()) {
+    _civicrm_api3_process_recruiter($dao->sort_name);
+    $returnValues[] = $dao->sort_name;
+  }
+  return civicrm_api3_create_success($returnValues, $params, 'SiActSource', 'Fix');
+}
 
-    // Spec: civicrm_api3_create_success($values = 1, $params = array(), $entity = NULL, $action = NULL)
-    return civicrm_api3_create_success($returnValues, $params, 'NewEntity', 'NewAction');
-  } else {
-    throw new API_Exception(/*errorMessage*/ 'Everyone knows that the magicword is "sesame"', /*errorCode*/ 1234);
+/**
+ * Function to keep only the latest recruiter and update all activities to that kept contact for all dupes, then trash dupes
+ * @param $sortName
+ */
+function _civicrm_api3_process_recruiter($sortName) {
+  $savedContactId = NULL;
+  $query = 'SELECT id FROM civicrm_contact WHERE sort_name = %1 AND contact_sub_type LIKE %2 ORDER BY id DESC';
+  $params = array(
+    1 => array((string) $sortName, 'String'),
+    2 => array('%recruiter%', 'String')
+  );
+  $dao = CRM_Core_DAO::executeQuery($query, $params);
+  while ($dao->fetch()) {
+    if (!$savedContactId) {
+      // first record is highest id, save that as it is the contact that will be kept
+      $savedContactId = $dao->id;
+    } else {
+
+      // find all activity contacts for contact and set them to saved contact
+      $update = "UPDATE civicrm_activity_contact SET contact_id = %1 WHERE contact_id = %2";
+      $updateParams = array(
+        1 => array($savedContactId, 'Integer'),
+        2 => array($dao->id, 'Integer')
+      );
+      CRM_Core_DAO::executeQuery($update, $updateParams);
+      // then trash contact
+      CRM_Core_DAO::executeQuery('UPDATE civicrm_contact SET is_deleted = %1 WHERE id = %2', array(
+        1 => array(1, 'Integer'),
+        2 => array($dao->id, 'Integer')
+      ));
+    }
   }
 }
 
